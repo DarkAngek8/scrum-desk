894492bb1da486ce08bcf393cc0046c6b9b926f5
DarkAngek8 <130296723+DarkAngek8@users.noreply.github.com>
2026-05-21 13:18:55 +0300

    set up login and registration by email

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { useForm } from "react-hook-form";
@@ -22,7 +24,7 @@ import { useLogin } from "../api/use-login";
 
 
 export const SignInCard = () => {
-  const { mutate } = useLogin();
+  const { mutate, isPending } = useLogin();
 
   const form = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
@@ -81,7 +83,7 @@ const onSubmit = async (values: z.infer<typeof loginSchema>) => {
               )}
             />
 
-            <Button disabled={false} size={"lg"} className="w-full">
+            <Button disabled={isPending} size={"lg"} className="w-full">
               Login
             </Button>
           </form>
@@ -92,7 +94,7 @@ const onSubmit = async (values: z.infer<typeof loginSchema>) => {
       </div>
       <CardContent className="p-7 flex flex-col gap-y-4">
         <Button
-          disabled={false}
+          disabled={isPending}
           variant={"secondary"}
           size={"lg"}
           className="w-full"
@@ -101,7 +103,7 @@ const onSubmit = async (values: z.infer<typeof loginSchema>) => {
           Login with Google
         </Button>
         <Button
-          disabled={false}
+          disabled={isPending}
           variant={"secondary"}
           size={"lg"}
           className="w-full"
