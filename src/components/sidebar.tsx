65154b435070debb32029fee73974a4e958ef93a
DarkAngek8 <130296723+DarkAngek8@users.noreply.github.com>
2026-05-21 14:40:50 +0300

    add sidebar layout(+mobile)

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image"
+import Link from "next/link"
+import { Separator } from "./separator"
+import { Navigator } from "./navigator"
+
+export const Sidebar = ()=>{
+    return(
+        <aside className="h-full bg-neutral-100 p-4 w-full">
+            <Link href="/">
+                <Image src="/logo.svg" alt="logo" width={164} height={48} />
+            </Link>
+            <Separator className="my-4" />
+            <Navigator />
+        </aside>
+    )
+}
