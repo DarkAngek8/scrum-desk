b67f9733b99c692a6b777711819181980ba7ac7c
DarkAngek8 <130296723+DarkAngek8@users.noreply.github.com>
2026-06-06 16:54:14 +0300

    fix bugs

--- src/features/workspaces/api/use-create-workspace.ts
+++ src/features/workspaces/api/use-create-workspace.ts
@@ -3,35 +3,31 @@ import { InferRequestType, InferResponseType } from "hono";
 import { toast } from "sonner";
 
 import { client } from "@/lib/rpc";
+import { useRouter } from "next/navigation";
 
+type ResponseType = InferResponseType<(typeof client.api.workspaces)["$post"]>;
+type RequestType = InferRequestType<(typeof client.api.workspaces)["$post"]>;
 
+export const useCreateWorkspace = () => {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ form }) => {
+      const response = await client.api.workspaces["$post"]({ form });
+      if (!response.ok) {
+        throw new Error("Failed to create workspace ");
+      }
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Wokspace created");
+      router.refresh();
+      queryClient.invalidateQueries({ queryKey: ["workspaces"] });
+    },
+    onError: () => {
+      toast.error("Failed to create workspace");
+    },
+  });
 
-type ResponseType = InferResponseType<typeof client.api.workspaces["$post"]>;
-type RequestType = InferRequestType<typeof client.api.workspaces["$post"]>;
-
-export const useCreateWorkspace = ()=>{
-    const queryClient = useQueryClient();
-    const mutation = useMutation<
-    ResponseType,
-    Error,
-    RequestType
-    >({
-        mutationFn: async ({form}) => {
-            const response = await client.api.workspaces["$post"]({form});
-            if (!response.ok) {
-                throw new Error("Failed to create workspace ");
-            };
-            return await response.json();
-        },
-        onSuccess: () =>{
-            toast.success("Wokspace created");
-            queryClient.invalidateQueries({queryKey:["workspaces"]})
-        },
-        onError: () => {
-        toast.error("Failed to create workspace");
-        }
-
-    });
-
-    return mutation;
+  return mutation;
 };
